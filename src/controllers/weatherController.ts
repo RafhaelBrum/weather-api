a14ebd821fe95dbb62dca0625b5d297ff90a083b
Rafhael <rafhael.brum@gmail.com>
2025-04-29 16:58:01 -0300

    cleaning code

--- src/controllers/weatherController.ts
+++ src/controllers/weatherController.ts
@@ -1,8 +1,5 @@
 import { Request, Response } from "express";
 
-const API_KEY = process.env.API_KEY as string;
-
-
 export async function getWeatherHandler(req: Request, res: Response) {
     try {
         const cityRaw = req.query.city;
