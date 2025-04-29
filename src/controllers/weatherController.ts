8622fc390449b2b3d0015541322bd6b1464e2d5c
Rafhael <rafhael.brum@gmail.com>
2025-04-29 16:24:02 -0300

    add GET weatherController

---
+++ src/controllers/weatherController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+
+const API_KEY = process.env.API_KEY as string;
+
+
+export async function getWeatherHandler(req: Request, res: Response) {
+    try {
+        const cityRaw = req.query.city;
+        if (!cityRaw) {
+            res.status(400).json({ error: 'City query parameter is required' });
+            return;
+        }
+        const city = String(cityRaw);
+
+        res.status(200).json({ message: `Searching ${ city } climate` });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
