7fc8652e4fd6e8fd94b6f4019257091e640c2f84
Rafhael <rafhael.brum@gmail.com>
2025-04-30 14:25:48 -0300

    integrating weather api service with controller

--- src/controllers/weatherController.ts
+++ src/controllers/weatherController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
+import { getWeather } from "../services/weatherService";
 
-export function getWeatherHandler(req: Request, res: Response) {
+export async function getWeatherHandler(req: Request, res: Response) {
     try {
         const cityRaw = req.query.city;
         if (!cityRaw) {
@@ -8,8 +9,10 @@ export function getWeatherHandler(req: Request, res: Response) {
             return;
         }
         const city = String(cityRaw);
+        const data = await getWeather(city);
+        console.log(data);
 
-        res.status(200).json({ message: `Searching ${ city } climate` });
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).json({ error: 'Internal server error' });
     }
